b856e42e5cb462126973580d819f8b0845777249
paweltar <paweltar@gmail.com>
2016-02-18 13:47:17 +0100

    ...

--- app/models/drama.rb
+++ app/models/drama.rb
@@ -15,17 +15,17 @@ class Drama < ActiveRecord::Base
 
 		title = doc.at_css('h1').text
 		last_ep_title = doc.css('#content-left li:nth-child(1) a').text
-		subbed_img_url = doc.css('li:nth-child(1) .raw').attr('src').to_s
-		subbed = subbed_img_url.include? "sub"
-		broadcast_date = doc.css('li:nth-child(1) span').text
-		poster = doc.css('.poster').attr('src').to_s
-		drama_description = doc.css('.info p').text
+		# subbed_img_url = doc.css('li:nth-child(1) .raw').attr('src').to_s
+		# subbed = subbed_img_url.include? "sub"
+		# broadcast_date = doc.css('li:nth-child(1) span').text
+		# poster = doc.css('.poster').attr('src').to_s
+		# drama_description = doc.css('.info p').text
 
 		self.title = title
-		self.img = poster
-		self.description = drama_description
-		self.subbed = subbed
-		self.broadcasted_at = Date.strptime(broadcast_date, "%A, %m/%d/%Y")
+		# self.img = poster
+		# self.description = drama_description
+		# self.subbed = subbed
+		# self.broadcasted_at = Date.strptime(broadcast_date, "%A, %m/%d/%Y")
 		self.last_ep = last_ep_title
 	end
 end
